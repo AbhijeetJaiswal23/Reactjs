5a7ac0006f414a327b2595e525ca2704e412355a
abhijeet <abhijeetak47aj@gmail.com>
2021-07-08 10:31:57 +0530

    Final Completation

--- shoppingcart/nameit/src/components/APP/App.jsx
+++ shoppingcart/nameit/src/components/APP/App.jsx
@@ -3,51 +3,49 @@ import Header from '../Header/Header';
 import './App.css';
 import Searchbar from '../Searchbar/Searchbar';
 import ReasultContainer from '../ReasultContainer/ReasultContainer';
+import { useState } from 'react';
 
 const name=require('@rstacruz/startup-name-generator');
 
-class App extends React.Component{
 
-    constructor(){
+const App =()=>{
 
-        super();
+    const [headerExpanded,setheaderExpanded]=useState(true);
+    const [suggestedNames,setsuggestedNames]=useState([]);
 
-        this.state={
 
-            headerExpanded:true,
-            suggestedNames:[]
-        };
+    
+    const handleinputChange=(inputText)=>{
+
+        // console.log(inputText);
+
+         setheaderExpanded(!inputText);
+         setsuggestedNames(inputText ? name(inputText) : []);
+
 
     }
+
+
+    return (
+        <div>
+            <Header headerExpanded={headerExpanded}/>
+            <Searchbar  oninputChange={handleinputChange} />
+            <ReasultContainer suggestedNames={suggestedNames} />
+        </div>
+    )
+
+
+
+}
+
+ 
 
    
 
 
-    handleinputChange=(inputText)=>{
 
-        // console.log(inputText);
-
-        this.setState({
-            
-            headerExpanded:!(inputText),
-            suggestedNames:name(inputText) //name it self gives an array so no need to push it like that
-    
-        })
-
-
-    }
-
-    render(){
-        
-        return (
-            <div>
-                <Header headerExpanded={this.state.headerExpanded}/>
-                <Searchbar  oninputChange={this.handleinputChange} />
-                <ReasultContainer suggestedNames={this.state.suggestedNames} />
-            </div>
-        )
-    }
-}
+     
+ 
 // function App (){
 
 //     return <h1>Hi welcome to app</h1>
